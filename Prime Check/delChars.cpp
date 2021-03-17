c75cd11d91f86f2ebdb31fefb05d68d3de584b02
XpulseIV <malteaxner363@gmail.com>
2021-03-17 19:06:28 +0100

    Update delChars.cpp

--- Prime Check/delChars.cpp
+++ Prime Check/delChars.cpp
@@ -2,34 +2,6 @@
 
 std::string remChars(std::string checkString)
 {
-	// All letters in alphabet (Lowercase)
-	checkString.erase(remove(checkString.begin(), checkString.end(), 'a'), checkString.end());
-	checkString.erase(remove(checkString.begin(), checkString.end(), 'b'), checkString.end());
-	checkString.erase(remove(checkString.begin(), checkString.end(), 'c'), checkString.end());
-	checkString.erase(remove(checkString.begin(), checkString.end(), 'd'), checkString.end());
-	checkString.erase(remove(checkString.begin(), checkString.end(), 'e'), checkString.end());
-	checkString.erase(remove(checkString.begin(), checkString.end(), 'f'), checkString.end());
-	checkString.erase(remove(checkString.begin(), checkString.end(), 'g'), checkString.end());
-	checkString.erase(remove(checkString.begin(), checkString.end(), 'h'), checkString.end());
-	checkString.erase(remove(checkString.begin(), checkString.end(), 'i'), checkString.end());
-	checkString.erase(remove(checkString.begin(), checkString.end(), 'j'), checkString.end());
-	checkString.erase(remove(checkString.begin(), checkString.end(), 'k'), checkString.end());
-	checkString.erase(remove(checkString.begin(), checkString.end(), 'l'), checkString.end());
-	checkString.erase(remove(checkString.begin(), checkString.end(), 'm'), checkString.end());
-	checkString.erase(remove(checkString.begin(), checkString.end(), 'n'), checkString.end());
-	checkString.erase(remove(checkString.begin(), checkString.end(), 'o'), checkString.end());
-	checkString.erase(remove(checkString.begin(), checkString.end(), 'p'), checkString.end());
-	checkString.erase(remove(checkString.begin(), checkString.end(), 'q'), checkString.end());
-	checkString.erase(remove(checkString.begin(), checkString.end(), 'r'), checkString.end());
-	checkString.erase(remove(checkString.begin(), checkString.end(), 's'), checkString.end());
-	checkString.erase(remove(checkString.begin(), checkString.end(), 't'), checkString.end());
-	checkString.erase(remove(checkString.begin(), checkString.end(), 'u'), checkString.end());
-	checkString.erase(remove(checkString.begin(), checkString.end(), 'v'), checkString.end());
-	checkString.erase(remove(checkString.begin(), checkString.end(), 'w'), checkString.end());
-	checkString.erase(remove(checkString.begin(), checkString.end(), 'x'), checkString.end());
-	checkString.erase(remove(checkString.begin(), checkString.end(), 'y'), checkString.end());
-	checkString.erase(remove(checkString.begin(), checkString.end(), 'z'), checkString.end());
-
 	// All letters in alphabet (Uppercase)
 	checkString.erase(remove(checkString.begin(), checkString.end(), 'A'), checkString.end());
 	checkString.erase(remove(checkString.begin(), checkString.end(), 'B'), checkString.end());
@@ -57,6 +29,34 @@ std::string remChars(std::string checkString)
 	checkString.erase(remove(checkString.begin(), checkString.end(), 'X'), checkString.end());
 	checkString.erase(remove(checkString.begin(), checkString.end(), 'Y'), checkString.end());
 	checkString.erase(remove(checkString.begin(), checkString.end(), 'Z'), checkString.end());
+
+	// All letters in alphabet (Lowercase)
+	checkString.erase(remove(checkString.begin(), checkString.end(), 'a'), checkString.end());
+	checkString.erase(remove(checkString.begin(), checkString.end(), 'b'), checkString.end());
+	checkString.erase(remove(checkString.begin(), checkString.end(), 'c'), checkString.end());
+	checkString.erase(remove(checkString.begin(), checkString.end(), 'd'), checkString.end());
+	checkString.erase(remove(checkString.begin(), checkString.end(), 'e'), checkString.end());
+	checkString.erase(remove(checkString.begin(), checkString.end(), 'f'), checkString.end());
+	checkString.erase(remove(checkString.begin(), checkString.end(), 'g'), checkString.end());
+	checkString.erase(remove(checkString.begin(), checkString.end(), 'h'), checkString.end());
+	checkString.erase(remove(checkString.begin(), checkString.end(), 'i'), checkString.end());
+	checkString.erase(remove(checkString.begin(), checkString.end(), 'j'), checkString.end());
+	checkString.erase(remove(checkString.begin(), checkString.end(), 'k'), checkString.end());
+	checkString.erase(remove(checkString.begin(), checkString.end(), 'l'), checkString.end());
+	checkString.erase(remove(checkString.begin(), checkString.end(), 'm'), checkString.end());
+	checkString.erase(remove(checkString.begin(), checkString.end(), 'n'), checkString.end());
+	checkString.erase(remove(checkString.begin(), checkString.end(), 'o'), checkString.end());
+	checkString.erase(remove(checkString.begin(), checkString.end(), 'p'), checkString.end());
+	checkString.erase(remove(checkString.begin(), checkString.end(), 'q'), checkString.end());
+	checkString.erase(remove(checkString.begin(), checkString.end(), 'r'), checkString.end());
+	checkString.erase(remove(checkString.begin(), checkString.end(), 's'), checkString.end());
+	checkString.erase(remove(checkString.begin(), checkString.end(), 't'), checkString.end());
+	checkString.erase(remove(checkString.begin(), checkString.end(), 'u'), checkString.end());
+	checkString.erase(remove(checkString.begin(), checkString.end(), 'v'), checkString.end());
+	checkString.erase(remove(checkString.begin(), checkString.end(), 'w'), checkString.end());
+	checkString.erase(remove(checkString.begin(), checkString.end(), 'x'), checkString.end());
+	checkString.erase(remove(checkString.begin(), checkString.end(), 'y'), checkString.end());
+	checkString.erase(remove(checkString.begin(), checkString.end(), 'z'), checkString.end());
 
 	return checkString;
 }
